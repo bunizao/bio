606a0251cccc78adf012a78e3618d831c9e3cf5e
bunizao <102936102+bunizao@users.noreply.github.com>
2025-08-06 02:30:43 +0800

    fix a typo

--- pages/index/_components/Footer.tsx
+++ pages/index/_components/Footer.tsx
@@ -4,7 +4,7 @@ export default () => {
       <p text-sm op-40>
         Built with 🩵 by <a href="https://github.com/bunizao" target="_blank" rel="noopener noreferrer" class="hover:opacity-80 transition-opacity">bunizao</a> © 2025
         <br />
-        Modified from <a href="https://ddiu.io" target="_blank" rel="noopener noreferrer" class="hover:opacity-80 transition-opacity">ddiu.io</a>. Published under MIT Licence.
+        Modified from <a href="https://ddiu.io" target="_blank" rel="noopener noreferrer" class="hover:opacity-80 transition-opacity">ddiu.io</a>. Published under MIT License.
       </p>
     </footer>
   )
